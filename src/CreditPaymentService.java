d80b4ca9ab6d78eca75ec218d42c411a2df3f9cc
Nikita Leonov <kontrofakt13@gmail.com>
2024-08-19 15:41:32 +0300

    first commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public double calculate(double amount, double percent, double term) {
         double calculate;
         term = term * 12;
-        percent = percent / 100 / term;
+        percent = percent / 100 / 12;
         calculate = amount * (percent / (1 -(Math.pow((1 + percent), -term))));
         return (int) calculate;
     }
